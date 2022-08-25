07988c9d3c85822c5dc6832d260b9422dfd4213b
navtsingh <navtsingh@ladmins-MacBook-Pro.local>
2022-08-25 12:58:12 -0700

    saved

---
+++ pages/use-context-optimized/index.js
@@ -0,0 +1,64 @@
+// Problemis whole tree is rendered.
+import {
+  useCount,
+  useUser,
+  useLogin,
+  useLogout,
+  useIncrement,
+  MyContextProvider,
+} from "./store";
+
+const DisplayUser = () => {
+  const user = useUser();
+  return <div>LogedIn: {user}</div>;
+};
+
+const Form = () => {
+  const login = useLogin();
+  const logOut = useLogout();
+
+  return (
+    <div>
+      <button onClick={login}>Login</button>
+      <button onClick={logOut}>Logout</button>
+    </div>
+  );
+};
+
+const Container = () => {
+  // container renders always
+  return <AddOneBttn />;
+};
+
+const Counter = () => {
+  const count = useCount();
+  return <div>Counter: {count}</div>;
+};
+
+const AddOneBttn = () => {
+  const increment = useIncrement();
+  return (
+    <div>
+      <button onClick={increment}>Increment</button>
+    </div>
+  );
+};
+
+const CounterUseState = () => {
+  return (
+    <div>
+      <Counter />
+      <AddOneBttn />
+      <DisplayUser />
+      <Form />
+    </div>
+  );
+};
+
+export default function Page() {
+  return (
+    <MyContextProvider>
+      <CounterUseState />
+    </MyContextProvider>
+  );
+}
---
+++ pages/use-context-optimized/store.js
@@ -0,0 +1,29 @@
+import React, { useCallback, useState } from "react";
+import { createContext, useContextSelector } from "use-context-selector";
+
+const useStore = () => {
+    const [user, setUser] = useState("");
+  const [count, setCount] = useState(0);
+  
+  return {
+    count,
+    user,
+    increment: useCallback(() => setCount((v) => v + 1), []),
+    login: useCallback(() => setUser("Navtej"), []),
+    logOut: useCallback(() => setUser(""), []),
+  };
+};
+
+const MyContext = createContext(null);
+
+export const MyContextProvider = ({ children }) => {
+  return <MyContext.Provider value={useStore()}>{children}</MyContext.Provider>;
+};
+
+// This is added to refer the values easily
+
+export const useCount = () => useContextSelector(MyContext, (s) => s.count);
+export const useIncrement = () => useContextSelector(MyContext, (s) => s.increment);
+export const useUser = () => useContextSelector(MyContext, (s) => s.user);
+export const useLogin = () => useContextSelector(MyContext, (s) => s.login);
+export const useLogout = () => useContextSelector(MyContext, (s) => s.logOut);
